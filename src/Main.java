83d61d6fc62db99bb25fec7367a28fb6423df844
SamehAtef <SZaher2022@outlook.com>
2023-01-23 22:24:58 +0200

    implement display Home page in main method until these point we can display List of students and display courses they enrolled in from file json

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -8,8 +9,24 @@ public class Main {
         //Students.displayData();
         //Students.displayData("4");
         //Courses.displayData("7");
-        Details.displayDetails();
+        //Details.displayDetails("44");
+        //System.out.println(Details.getCourses("45"));
+        Scanner scanner  = new Scanner(System.in);
+        displayHomepage();
+        Students.displayData();
+        System.out.println("------------------------------------------------------------------------------------");
+        System.out.println("Please Select the required student: ");
+        String student_id=scanner.next();
+        Details.displayDetails(student_id);
 
+    }
+    private static void displayHomepage(){
+        final String Name = "Sameh Atef";
+        System.out.println("Welcome to LMS");
+        System.out.println("Create by: "+"{"+Name +"}");
+        System.out.println("====================================================================================");
+        System.out.println("Home page");
+        System.out.println("====================================================================================");
 
     }
 
