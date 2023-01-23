de887a29799920a539d2190b4714bab7aca147a9
SamehAtef <SZaher2022@outlook.com>
2023-01-23 21:48:40 +0200

    new feature add to Student Class to improve display data

--- src/Students.java
+++ src/Students.java
@@ -8,10 +8,8 @@ import java.util.regex.Pattern;
 
 public class Students extends Data {
 // method to display student-data.csv
-
     public static void displayData() throws IOException {
         List<String>Student=new ArrayList<>();
-
         // creates scanner of that file path
         //Scanner scan = new Scanner(new File("src/data/student-data.csv"));
         // read till there aren't elements
@@ -23,14 +21,12 @@ public class Students extends Data {
         displayHeader();
 
         while ((line = reader.readLine()) != null) {
-
             // use comma as separator
             String[] fields = line.split(",");
             formatData(fields);
 
             System.out.println();
         }
-
     }
     public static void convertData(String fileName,String type){
         final File input = new File("src/data/"+fileName+"."+type);
@@ -65,13 +61,10 @@ public class Students extends Data {
     // function used to format our csv data accord to requirements
     private static void formatData(String [] fields  ) {
 
-        for (String field : fields) {
             int i =0;
                 System.out.printf("%-5s%-20s%-15s%-37s%-35s%-30s%-25s",
                         fields[i],fields[i+1],fields[i+2],fields[i+3],fields[i+4],fields[i+5],fields[i+6]);
-                break;
 
-        }
     }
 private static void formatData(String [] fields, String id  ) {
         //System.out.printf("%-25s",fields[0]);
@@ -80,11 +73,14 @@ private static void formatData(String [] fields, String id  ) {
 //search for pattern that can match only digit
                 int i = 1;
                 if (field.equals((id))) {
-                    System.out.printf("%-10s%15s%10s%30s",field,fields[i],fields[i+1],fields[i+2]);
+                    System.out.printf("%-10s%-30s%-10s%-30s",field,fields[i],fields[i+1],fields[i+2]);
+                    System.out.println();
+
 
                 } else {
                     break;
                 }
+
 
             }
 
@@ -93,16 +89,13 @@ private static void formatData(String [] fields, String id  ) {
 
 
         }
-
-
-
-
+        //display Header as a title
         private static void displayHeader(){
             System.out.println("---------------------------------------------------------");
             System.out.println("Current Student List");
             System.out.println("---------------------------------------------------------");
         }
-
+    // display Data of Students file csv
 
      public static void displayData(String id) throws IOException {
         List<String>Student=new ArrayList<>();
@@ -116,15 +109,25 @@ private static void formatData(String [] fields, String id  ) {
 
         reader = new BufferedReader(new FileReader(input));
         String line = null;
+         displayHeader1();
 
         while ((line = reader.readLine()) != null ) {
 
             // use comma as separator
             String[] fields = line.split(",");
-
-            formatData(fields,"7");
+            formatData(fields,id);
             //System.out.println();
         }
+
+    }
+    private static void displayHeader1(){
+        int i=0;
+        //System.out.printf()
+        System.out.printf("%-10s%-30s%-10s%-30s","id","name","grade","e-mail");
+        System.out.println();
+
+
+
 
     }
 
